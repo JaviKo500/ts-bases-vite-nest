4aef99b17f2b4d55d2062a93dc0455494ec555ff
JaViKo500 <javikogutierrez64@gmail.com>
2024-06-08 22:06:06 -0500

    init project

---
+++ src/bases/01-types.ts
@@ -0,0 +1,21 @@
+export let name = 'JK500';
+export const age = 35;
+export const isValid: boolean = true;
+
+name = 'Jj-500'
+
+
+export const templateString = `
+   this is a string multi-line
+
+   "" double
+   ' simple
+
+   inject values ${ 1 + 1 }
+   numbers: ${ age }
+   booleans: ${ isValid }
+`;
+
+
+
+
